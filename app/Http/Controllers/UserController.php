f10e3e5cfb6d2cab828ea947a8d077aff3b847ef
ezjeep <ezjeep1@gmail.com>
2023-02-14 12:36:49 +0800

    for class, from h

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        return 'Hello from users';
+    }
+
+    public function show($id){
+        $data=array(
+            "id"=>$id,
+            "name"=>"BarryCabaluna",
+            "age"=>21,
+            "email"=>"janba@gmail.com");
+            return view('user', $data); 
+    }
+}
